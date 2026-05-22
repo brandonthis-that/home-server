b8421e63e7a4d56bbaed4907604edfc6530f8c7f
brando <brandonnyakea@gmail.com>
2026-05-22 17:42:36 +0300

    list all kernels

--- get_info.go
+++ get_info.go
@@ -10,11 +10,26 @@ func get_kernel() string {
 	kernel_file, err := os.Open("/proc/version")
 	if err != nil {
 		fmt.Println("Error", err)
-		os.Exit(1)
+		os.Exit(0)
 	}
 	kernel_info := make([]byte, 1024)
 	kernel_file.Read(kernel_info)
 	kernel_file.Close()
 	kernel_list := strings.Split(string(kernel_info), " ")
 	return strings.TrimSpace(kernel_list[2])
+}
+
+func get_all_kernels() []string {
+	entries, err := os.ReadDir("/boot")
+	if err != nil {
+		fmt.Println("Error", err)
+		os.Exit(0)
+	}
+	var kernels []string
+	for _, entry := range entries {
+		if strings.HasPrefix(entry.Name(), "vmlinuz") {
+			kernels = append(kernels, entry.Name())
+		}
+	}
+	return kernels
 }
--- main.go
+++ main.go
@@ -6,5 +6,10 @@ import (
 
 func main() {
 	kernel_version := get_kernel()
+	allKernels := get_all_kernels()
 	fmt.Println(kernel_version)
+	for _, k := range allKernels {
+		fmt.Println(k)
+	}
+	fmt.Println("All good")
 }
